08a5c65acf2be5ac5e6538721380b50c903e5be6
Brian Nenninger <bnenning@gmail.com>
2021-07-16 01:21:41 -0400

    Start of UI

---
+++ assets/doc/about.md
@@ -0,0 +1,18 @@
+Comments or bug reports: [bnenning@gmail.com](mailto:bnenning@gmail.com)
+
+## Rules
+
+...
+
+## License
+
+This application is released under the GNU General Public License, version 3. Source code is
+available [here](https://github.com/dozingcat/MousePounce).
+
+## Credits
+
+- Cats and paws by [AnnaliseArt on Pixabay](https://pixabay.com/illustrations/cats-hanging-cats-kitty-cat-paw-3611310/)
+- Thought bubble by [OpenClipart-Vectors on Pixabay](https://pixabay.com/vectors/balloon-bubble-speech-thought-150981/)
+- "No" symbol by [OpenClipart-Vectors on Pixabay](https://pixabay.com/vectors/unauthorised-denied-ban-prohibition-156169/)
+- Cat emojis from [Noto Emoji by Google](https://github.com/googlefonts/noto-emoji/)
+- [Playing card images](https://code.google.com/archive/p/vector-playing-cards/)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:hearts/cards/rollout.dart';
+import 'package:hearts/hearts/hearts.dart';
+import 'package:hearts/hearts/hearts_ai.dart';
+
+import 'cards/card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,24 +14,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CatTricks',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'CatTricks'),
     );
   }
 }
@@ -32,84 +29,174 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+enum AnimationMode {
+  none,
+  playing_trick_card,
+  moving_trick_to_winner,
+}
 
-  void _incrementCounter() {
+class Layout {
+  late Size displaySize;
+  late double edgePx;
+
+  Rect cardArea() {
+    return Rect.fromLTRB(edgePx, edgePx, displaySize.width - edgePx, displaySize.height - edgePx);
+  }
+
+  Rect areaForTrickCard(int playerIndex) {
+    final ca = cardArea();
+    final cardHeight = ca.height * 0.4;
+    final cardWidth = ca.width * 0.3;
+    final centerXFrac = (playerIndex == 1) ?
+        0.25 :
+        (playerIndex == 3) ? 0.75 : 0.5;
+    final centerYFrac = (playerIndex == 0) ?
+        0.75 :
+        (playerIndex == 2) ? 0.25 : 0.5;
+    final centerX = ca.left + ca.width * centerXFrac;
+    final centerY = ca.top + ca.height * centerYFrac;
+    return Rect.fromLTWH(centerX - cardWidth / 2, centerY - cardHeight / 2, cardWidth, cardHeight);
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final rng = Random();
+  final rules = HeartsRuleSet();
+  var animationMode = AnimationMode.none;
+  late HeartsRound round;
+
+  @override void initState() {
+    round = HeartsRound.deal(rules, List.filled(4, 0), 0, rng);
+    Future.delayed(Duration(milliseconds: 500), () => _playNextCard());
+  }
+
+  void _playNextCard() {
+    if (round.isOver()) {
+      round = HeartsRound.deal(rules, List.filled(4, 0), 0, rng);
+    }
+    // Do this in a separate thread/isolate.
+    final card = chooseCardMonteCarlo(
+        CardToPlayRequest.fromRound(round),
+        MonteCarloParams(numHands: 20, rolloutsPerHand: 50),
+        chooseCardAvoidingPoints,
+        rng);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      round.playCard(card);
     });
+    Future.delayed(Duration(milliseconds: 500), () => _playNextCard());
+  }
+
+  Widget _cardForPlayer(final Layout layout, final PlayingCard card, int playerIndex) {
+    final cardRect = layout.areaForTrickCard(playerIndex);
+    final cardImagePath = "assets/cards/${card.toString()}.webp";
+    return Positioned(
+      left: cardRect.left,
+      top: cardRect.top,
+      width: cardRect.width,
+      height: cardRect.height,
+      child: Container(
+          child: Image(
+              image: AssetImage(cardImagePath),
+              fit: BoxFit.contain,
+              alignment: Alignment.center,
+      )));
+  }
+
+  Widget _trickCards(final Layout layout) {
+    List<Widget> cardWidgets = [];
+    if (round.currentTrick.cards.isNotEmpty) {
+      for (int i = 0; i < round.currentTrick.cards.length; i++) {
+        int p = (round.currentTrick.leader + i) % round.rules.numPlayers;
+        cardWidgets.add(_cardForPlayer(layout, round.currentTrick.cards[i], p));
+      }
+    }
+    else if (round.previousTricks.isNotEmpty) {
+      final trick = round.previousTricks.last;
+      for (int i = 0; i < trick.cards.length; i++) {
+        int p = (trick.leader + i) % round.rules.numPlayers;
+        cardWidgets.add(_cardForPlayer(layout, trick.cards[i], p));
+      }
+    }
+    return Stack(children: cardWidgets);
+  }
+
+  Widget _aiPlayerWidget(final Layout layout, int playerIndex) {
+    final imagePath = "assets/cats/cat${playerIndex + 1}.png";
+    final imageAspectRatio = 156 / 112;
+    final displaySize = layout.displaySize;
+    final playerSize = layout.edgePx;
+
+    final rect = (() {
+      switch (playerIndex) {
+        case 0:
+          return Rect.fromLTWH(0, displaySize.height - playerSize, displaySize.width, playerSize);
+        case 1:
+          return Rect.fromLTWH(0, 0, playerSize, displaySize.height);
+        case 2:
+          return Rect.fromLTWH(0, 0, displaySize.width, playerSize);
+        case 3:
+          return Rect.fromLTWH(displaySize.width - playerSize, 0, playerSize, displaySize.height);
+        default:
+          return Rect.fromLTWH(0, 0, 0, 0);
+      }
+    })();
+    final angle = (playerIndex - 2) * pi / 2;
+    final scale = (playerIndex == 1 || playerIndex == 3) ? imageAspectRatio : 1.0;
+
+    return Positioned(
+      top: rect.top,
+      left: rect.left,
+      width: rect.width,
+      height: rect.height,
+
+      child: Container(
+        color: Colors.white70,
+        width: rect.width,
+        height: rect.height,
+        // The image won't naturally take up the full width If rotated 90 degrees,
+        // so in that case scale by the aspect ratio.
+        child: Transform.scale(scale: scale, child: Transform.rotate(angle: angle, child: Image(
+          image: AssetImage(imagePath),
+          fit: BoxFit.contain,
+          alignment: Alignment.center,
+        ))),
+      ),
+
+      // child: Text("Hello $playerNum"),
+    );
+  }
+
+  Layout computeLayout() {
+    final ds = MediaQuery.of(context).size;
+    return Layout()
+        ..displaySize = ds
+        ..edgePx = max(ds.width / 20, ds.height / 15)
+        ;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final layout = computeLayout();
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.green,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ...[0, 1, 2, 3].map((i) => _aiPlayerWidget(layout, i)),
+            _trickCards(layout),
+
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
