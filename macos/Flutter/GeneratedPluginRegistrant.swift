9c997414a4ed027f23590b0f42a79b63afb820ca
Brian Nenninger <bnenning@gmail.com>
2023-02-13 22:41:17 -0500

    Update dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,8 @@ import Foundation
 
 import audio_session
 import just_audio
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
